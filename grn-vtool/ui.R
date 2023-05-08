a6a0516a33c66bf48f769a9fc0af45fb0b9268bd
Nora Alanzi <51384420+4n8x@users.noreply.github.com>
2023-05-08 21:21:54 +0300

    Update ui.R
    new button

--- grn-vtool/ui.R
+++ grn-vtool/ui.R
@@ -13,21 +13,6 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+    titlePanel("GRN-Vtool"),
+    actionButton("SeqNet", "Click Me")
 )
