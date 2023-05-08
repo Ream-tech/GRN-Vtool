160a02eb55c09de26071a3e7576b7842282744aa
RawanAlhabib <132562897+RawanAlhabib@users.noreply.github.com>
2023-05-08 21:36:52 +0300

    Update ui.R
    Here I created the interface of the dashboard

--- ui.R
+++ ui.R
@@ -8,11 +8,32 @@
 #
 
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("GRN-Vtool"),
-    actionButton("SeqNet", "Click Me")
+ui <- dashboardPage(
+  dashboardHeader(title = "GRN VTOOLS"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Data import", tabName = "Data import", icon = icon("th")),
+      fileInput("file1", "Choose a file"),
+      menuItem("Normalisation", tabName = "Normalisation", icon = icon("th")),
+      menuItem("Exploratory analysis", tabName = "Exploratory analysis", icon = icon("th")),
+      menuItem("Differntial Expression", tabName = "Differntial Expression", icon = icon("th")),
+      selectInput("Distibution","Please Select a Tool:", choices =c("SeqNet", "DIANE")),
+      menuItem("Network Generation", tabName = "Network Generation", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tableOutput("content")
+  )
 )
+
+server <- function(input, output) {
+  output$content <- renderTable({
+    infile <- input$file1
+    if (is.null(infile)) return(NULL)
+    read.csv(infile$datapath)
+  })
+}
+
+shinyApp(ui, server)
