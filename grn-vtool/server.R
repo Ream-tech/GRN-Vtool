b0a88c8c11ed3ffbfd35bda0fbbf3b631ee3d29c
Nora Alanzi <51384420+4n8x@users.noreply.github.com>
2023-05-08 21:22:21 +0300

    Update server.R

--- grn-vtool/server.R
+++ grn-vtool/server.R
@@ -7,22 +7,19 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+
+
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
+ 
+  p <- 100
+   network_1 <- random_network(p)
+   network_2 <- as_single_module(network_1)
+   g<-plot(network_1)
+  observeEvent(input$SeqNet, {
+  output$p1<-renderPlot({plot(g)})
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
+  })
 
 }
